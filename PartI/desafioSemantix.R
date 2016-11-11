917f199e848154c47ed9dfe7f4e26bb08c3b97b7
Rui Romanini <ruiromanini@msn.com>
2016-11-11 15:02:27 -0200

    Add files via upload
    Arquivo .R com códigos para download, exploração e limpeza dos dados, plot da rede e resposta com as métricas de "closeness".

--- PartI/desafioSemantix.R
+++ PartI/desafioSemantix.R
@@ -3,18 +3,25 @@ if(!require(igraph)){
   library(igraph)
 }
 
-#Download do arquivo contendo os links
+#Download the edges file
 destfile="./edges.dat"
 fileURL = "https://raw.githubusercontent.com/rromanini/DesafioSemantix/master/PartI/edges.dat";
 download.file(fileURL,destfile=destfile,method="auto")
 
-#Leitura dos vertices
+#Reading the vertices
 vertices <- read.csv("./edges.dat" ,sep = " ",header = FALSE)
 
-#Criação da rede a partir dos vertices
+#Exploring and cleaning data
+summary(vertices)
+
+#Looking for missing values
+length(which(is.na(vertices) == TRUE))
+
+
+#Creating the network from the vertices
 net <-graph_from_data_frame(d=vertices,directed = T)
 
-#Plotagem da rede
+#Plotting the network
 l <- layout_with_fr(net)
 l <- norm_coords(l, ymin=-1, ymax=1, xmin=-1, xmax=1)
 
@@ -24,5 +31,11 @@ plot(net,edge.arrow.size=.05,vertex.size=10,
      vertex.color="green",
      vertex.label.cex=0.5,rescale=F, layout=l*1.2)
 
-#Calculo de "closeness"
-closeness(net, mode="out")
+#Getting the closeness
+closeness_matrix <- as.matrix(closeness(net, mode="out"))
+closeness_matrix <- as.matrix(closeness_matrix[order(closeness_matrix[,1],decreasing = TRUE),])
+
+#Add column name  
+colnames(closeness_matrix) <- c("Closeness")
+
+closeness_matrix
